9e3cea68ddfbff321cb13779219d39fa6d82aaca
siriusdemon <mory2016@126.com>
2021-11-12 11:18:22 +0800

    remove out-of-date thing

--- llvm/README.md
+++
@@ -1,4 +0,0 @@
-# Rust One Piece on llvm
-
-
-+ start from [here](https://llvm.org/docs/tutorial/MyFirstLanguageFrontend/LangImpl01.html)
--- llvm/llpy/src/syntax.rs
+++
@@ -1,7 +0,0 @@
-pub enum Expr {
-    Num(f64),
-    Var(String),
-    Prim2(String, Box<Expr>, Box<Expr>),
-    Call(String, Vec<Expr>),
-    
-}
